fcb34ecf8e4c340c0f8a3c40c362be149e745937
MalikShahmir <malikshahmir098@gmail.com>
2026-07-14 00:09:31 +0500

    Refactor pattern printing logic to implement Pascal's triangle and improve output formatting

--- PattrenExercise/pattren.cpp
+++ PattrenExercise/pattren.cpp
@@ -17,16 +17,43 @@ int main()
     //     cout << endl;
     // }
 
-    int n;
-    int num1 = 0;
-    cout << "Enter the number of rows: ";
-    cin >> n;
+
+
+    // int n;
+    // int num1 = 0;
+    // cout << "Enter the number of rows: ";
+    // cin >> n;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0 ; j < n; j++)
+    //     {
+    //         cout << (num1 + 1) << " ";
+    //         num1++;
+    //     }
+    //     cout << endl;
+    // }
+
+
+// pattren  pascal triangle
+    int n = 4;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0 ; j < n; j++)
+        // space :n-i-1
+        for (int j = 0 ; j < n-i-1; j++)
         {
-            cout << (num1 + 1) << " ";
-            num1++;
+            cout<< " ";
+        }
+
+        // num1: i+1
+        for (int k = 1; k <= i+1; k++)
+        {   
+            cout << k;
+        }
+
+        // num2: i
+        for (int l = i; l >= 1; l--)
+        {
+            cout << l;
         }
         cout << endl;
     }
