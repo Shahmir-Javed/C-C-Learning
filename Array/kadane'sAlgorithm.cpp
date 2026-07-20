dac121df4bb6273679bafa292f7959c0c4996af0
MalikShahmir <malikshahmir098@gmail.com>
2026-07-20 16:37:32 +0500

    Refactor Kadane's algorithm implementation and add two-sum solution using vectors

--- Array/kadane'sAlgorithm.cpp
+++ Array/kadane'sAlgorithm.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <climits>
+#include <vector>
 using namespace std;
 
 int main()
@@ -20,12 +21,11 @@ int main()
     //     cout << endl;
     // }
 
-
     // Brute Force Approach to find the maximum contiguous sum in an array
     // int n = 5;
     // int arr[5] = {1, 2, 3, 4, 5};
     // int maxSum = 0; // Initialize maxSum to the smallest possible integer
-    
+
     // for (int st = 0; st < n; st++)
     // {
     //     int currentSum = 0; // Initialize currentSum for each starting index
@@ -39,20 +39,39 @@ int main()
 
     // cout << "Maximum contiguous sum: " << maxSum << endl;
 
-
     // Kadane's Algorithm to find the maximum contiguous sum in an array
-    int n = 5;
-    int arr[5] = {1, 2, 3, 4, 5};
-    int maxSum =INT_MIN; // Initialize maxSumKadane to the smallest possible integer
-    int currentSum = 0; // Initialize currentSum for Kadane's algorithm
+    // int n = 5;
+    // int arr[5] = {1, 2, 3, 4, 5};
+    // int maxSum =INT_MIN; // Initialize maxSumKadane to the smallest possible integer
+    // int currentSum = 0; // Initialize currentSum for Kadane's algorithm
 
+    // for (int i = 0; i < n; i++)
+    // {
+    //     currentSum = max(arr[i], currentSum + arr[i]); // Update currentSum
+    //     maxSum = max(maxSum, currentSum); // Update maxSum if currentSum is greater
+    // }
+
+    // cout << "Maximum contiguous sum (Kadane's Algorithm): " << maxSum << endl;
+
+    vector<int> num = {5, 11, 2, 7};
+    int target = 9;
+    int n = num.size();
+    vector<int> ans;
     for (int i = 0; i < n; i++)
     {
-        currentSum = max(arr[i], currentSum + arr[i]); // Update currentSum
-        maxSum = max(maxSum, currentSum); // Update maxSum if currentSum is greater
+        for (int j = i + 1; j < n; j++)
+        {
+            if (num[i] + num[j] == target)
+            {
+                ans.push_back(i);
+                ans.push_back(j);
+                break;
+            }
+        }
     }
-
-    cout << "Maximum contiguous sum (Kadane's Algorithm): " << maxSum << endl;
-
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
     return 0;
 }
