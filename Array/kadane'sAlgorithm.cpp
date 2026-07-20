de714a271259e3bacb8325179b42f8d7f9894e78
MalikShahmir <malikshahmir098@gmail.com>
2026-07-20 16:12:14 +0500

    Refactor Kadane's algorithm implementation for maximum contiguous sum in an array

--- Array/kadane'sAlgorithm.cpp
+++ Array/kadane'sAlgorithm.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 
 int main()
@@ -21,21 +22,37 @@ int main()
 
 
     // Brute Force Approach to find the maximum contiguous sum in an array
+    // int n = 5;
+    // int arr[5] = {1, 2, 3, 4, 5};
+    // int maxSum = 0; // Initialize maxSum to the smallest possible integer
+    
+    // for (int st = 0; st < n; st++)
+    // {
+    //     int currentSum = 0; // Initialize currentSum for each starting index
+    //     for (int en = st; en < n; en++)
+    //     {
+    //         currentSum += arr[en];  // Add the current element to currentSum
+    //         maxSum = max(maxSum, currentSum); // Update maxSum if currentSum is greater
+    //         cout << currentSum << "  ";
+    //     }
+    // }
+
+    // cout << "Maximum contiguous sum: " << maxSum << endl;
+
+
+    // Kadane's Algorithm to find the maximum contiguous sum in an array
     int n = 5;
     int arr[5] = {1, 2, 3, 4, 5};
-    int maxSum = 0; // Initialize maxSum to the smallest possible integer
+    int maxSum =INT_MIN; // Initialize maxSumKadane to the smallest possible integer
+    int currentSum = 0; // Initialize currentSum for Kadane's algorithm
 
-    for (int st = 0; st < n; st++)
+    for (int i = 0; i < n; i++)
     {
-        int currentSum = 0; // Initialize currentSum for each starting index
-        for (int en = st; en < n; en++)
-        {
-            currentSum += arr[en];  // Add the current element to currentSum
-            maxSum = max(maxSum, currentSum); // Update maxSum if currentSum is greater
-            cout << currentSum << "  ";
-        }
+        currentSum = max(arr[i], currentSum + arr[i]); // Update currentSum
+        maxSum = max(maxSum, currentSum); // Update maxSum if currentSum is greater
     }
 
-    cout << "Maximum contiguous sum: " << maxSum << endl;
+    cout << "Maximum contiguous sum (Kadane's Algorithm): " << maxSum << endl;
+
     return 0;
 }
